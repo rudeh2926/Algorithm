a6c2b13bf5d46258ef16af7f6e8ebc8829f55a4f
도경 <125863754+rudeh2926@users.noreply.github.com>
2023-12-13 14:33:01 +0900

    Update Contact.py

--- 백준/Gold/1013. Contact/Contact.py
+++ 백준/Gold/1013. Contact/Contact.py
@@ -1,1 +1,14 @@
+import sys
+import re
+
+a = re.compile('(100+1+|01)+')
+for i in range(int(sys.stdin.readline())):
+   b = input()
+   if a.fullmatch(b):
+       print("YES")
+   else:
+       print("NO")
+
+
+// 한줄
 import re, sys; print('\n'.join(['YES' if re.fullmatch('(100+1+|01)+', input()) else 'NO' for _ in range(int(sys.stdin.readline()))]))
