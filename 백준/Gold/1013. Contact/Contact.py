acc69ae41562c83d981d3fb53b946e0ab6d9ecb1
도경 <125863754+rudeh2926@users.noreply.github.com>
2023-12-13 14:04:45 +0900

    [Gold V] Title: Contact, Time: 200 ms, Memory: 34720 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.py
@@ -0,0 +1,10 @@
+import sys
+import re
+
+p = re.compile('(100+1+|01)+')
+for i in range(int(sys.stdin.readline())):
+   s = input().strip()
+   if p.fullmatch(s):
+       print("YES")
+   else:
+       print("NO")
