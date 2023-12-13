1a1cbb66edbe7e7fa7226278b5f14fe1cebb3eb2
도경 <125863754+rudeh2926@users.noreply.github.com>
2023-12-13 14:32:20 +0900

    [Gold V] Title: Contact, Time: 148 ms, Memory: 34728 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.py
+++ 백준/Gold/1013. Contact/Contact.py
@@ -1,10 +1,1 @@
-import sys
-import re
-
-a = re.compile('(100+1+|01)+')
-for i in range(int(sys.stdin.readline())):
-   b = input()
-   if a.fullmatch(b):
-       print("YES")
-   else:
-       print("NO")
+import re, sys; print('\n'.join(['YES' if re.fullmatch('(100+1+|01)+', input()) else 'NO' for _ in range(int(sys.stdin.readline()))]))
