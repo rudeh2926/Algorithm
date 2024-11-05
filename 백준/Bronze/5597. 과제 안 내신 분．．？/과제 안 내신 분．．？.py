19127ddef67f94cae8d7671b41598ce5f989e99c
도경 <125863754+rudeh2926@users.noreply.github.com>
2024-11-05 23:38:14 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,14 +1,6 @@
-a = []
-null = []
-for _ in range(1,29):
+a = [i for i in range(1, 31)]
+for _ in range(28):
     b = int(input())
-    a.append(b)
-
-
-for i in range(1,31):
-    if i not in a:
-        null.append(i)
-    else:
-        continue
-print(min(null))
-print(max(null))
+    a.remove(b)
+print(min(a))
+print(max(a))
