fe2eb02a6f2b5e5ad838dc47981cbe739fdf9f8f
도경 <125863754+rudeh2926@users.noreply.github.com>
2025-05-02 10:26:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,12 @@
-a = int(input())
-c = 0
-while a >= 0:
-    if a % 5 == 0:
-        c += int(a // 5)
-        print(c)
+weight = int(input())
+result = 0
+while weight >= 0:
+    if weight % 5 == 0:
+        result += weight // 5
+        print(result)
         break
-    a -= 3
-    c += 1
+    else:
+        weight -= 3
+        result += 1
 else:
     print(-1)
